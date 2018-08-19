adaa166717a8bed1a65d3ac2c206ef0c532693a1
jayaraj19 <jayaraj.cse.97@gmail.com>
2018-08-19 20:47:40 +0530

    minute changes made

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from jobs import urls, views
+from jobs import views
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', views.index),
-                  path('jobs/', include(urls))
+                  path('jobs/', include('jobs.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
